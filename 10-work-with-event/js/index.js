51d4ceabd3e677f57d1ddada31d5743fd675d609
Serhii <vasylyk29@gmail.com>
2020-03-03 13:30:24 +0200

    done hw 10

---
+++ 10-work-with-event/js/index.js
@@ -0,0 +1,57 @@
+const note = document.querySelectorAll('.sound-item');
+const noteDo = document.querySelector('.sound-do');
+const noteRe = document.querySelector('.sound-re');
+const noteMi = document.querySelector('.sound-mi');
+const noteFa = document.querySelector('.sound-fa');
+const noteSol = document.querySelector('.sound-sol');
+const noteLja = document.querySelector('.sound-lja');
+const noteSi = document.querySelector('.sound-si');
+
+note.forEach(item => {
+  item.addEventListener( "click" , function() {
+    const audio = this.querySelector('audio')
+    audio.play()
+  });
+})
+
+document.addEventListener('keypress', (event) => {
+  switch(event.keyCode) {
+    case 113 :
+    case 1081 :
+      noteDo.play()
+      break;
+
+    case 119 :
+    case 1094 :
+      noteRe.play()
+      break;
+
+    case 101 :
+    case 1091 :
+      noteMi.play()
+      break;
+
+    case 114 :
+    case 1082 :
+      noteFa.play()
+      break;
+
+    case 116 :
+    case 1077 :
+      noteSol.play()
+      break;
+
+    case 121 :
+    case 1085 :
+      noteLja.play()
+      break;
+
+    case 117 :
+    case 1075 :
+      noteSi.play()
+      break;
+
+    default:
+      break
+  }
+});
