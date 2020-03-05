76bd99b21011333df277efa46f0d37ce2125c881
Serhii <vasylyk29@gmail.com>
2020-03-05 13:55:36 +0200

    fix

--- 10-work-with-event/js/index.js
+++ 10-work-with-event/js/index.js
@@ -6,6 +6,8 @@ const noteFa = document.querySelector('.sound-fa');
 const noteSol = document.querySelector('.sound-sol');
 const noteLja = document.querySelector('.sound-lja');
 const noteSi = document.querySelector('.sound-si');
+// const dataKeyCodeRu = document.getAttribute
+// console.log(dataKeyCodeRu)
 
 note.forEach(item => {
   item.addEventListener( "click" , function() {
@@ -15,44 +17,56 @@ note.forEach(item => {
   });
 })
 
-document.addEventListener('keypress', (event) => {
-  switch(event.keyCode) {
-    case 113 :
-    case 1081 :
-      noteDo.play()
-      break;
+document.addEventListener('keypress', (e) => {
+  console.log(e.keyCode)
+  note.forEach((item) => {
+    
+    if(Number(item.dataset.keycodeRu) === e.keyCode ||
+     Number(item.dataset.keycodeEng) === e.keyCode){
+      const audio = item.querySelector('audio')
+      audio.currentTime = 0;
+      audio.play()
+    }
+  })
 
-    case 119 :
-    case 1094 :
-      noteRe.play()
-      break;
 
-    case 101 :
-    case 1091 :
-      noteMi.play()
-      break;
+  // switch(event.keyCode) {
+  //   case 113 :
+  //   case 1081 :
+  //     noteDo.play()
+  //     break;
 
-    case 114 :
-    case 1082 :
-      noteFa.play()
-      break;
+  //   case 119 :
+  //   case 1094 :
+  //     noteRe.play()
+  //     break;
 
-    case 116 :
-    case 1077 :
-      noteSol.play()
-      break;
+  //   case 101 :
+  //   case 1091 :
+  //     noteMi.play()
+  //     break;
 
-    case 121 :
-    case 1085 :
-      noteLja.play()
-      break;
+  //   case 114 :
+  //   case 1082 :
+  //     noteFa.play()
+  //     break;
 
-    case 117 :
-    case 1075 :
-      noteSi.play()
-      break;
+  //   case 116 :
+  //   case 1077 :
+  //     noteSol.play()
+  //     break;
 
-    default:
-      break
-  }
+  //   case 121 :
+  //   case 1085 :
+  //     noteLja.play()
+  //     break;
+
+  //   case 117 :
+  //   case 1075 :
+  //     noteSi.play()
+  //     break;
+
+  //   default:
+  //     break
+  // }
 });
