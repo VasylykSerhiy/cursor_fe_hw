c4fcb8a64c847a8eaa45d76806535f09ffa9a0ca
Serhii <vasylyk29@gmail.com>
2020-03-05 12:30:00 +0200

    fix

--- 10-work-with-event/js/index.js
+++ 10-work-with-event/js/index.js
@@ -10,6 +10,7 @@ const noteSi = document.querySelector('.sound-si');
 note.forEach(item => {
   item.addEventListener( "click" , function() {
     const audio = this.querySelector('audio')
+    audio.currentTime = 0;
     audio.play()
   });
 })
@@ -18,43 +19,36 @@ document.addEventListener('keypress', (event) => {
   switch(event.keyCode) {
     case 113 :
     case 1081 :
-      audio.currentTime = 0;
       noteDo.play()
       break;
 
     case 119 :
     case 1094 :
-      audio.currentTime = 0;
       noteRe.play()
       break;
 
     case 101 :
     case 1091 :
-      audio.currentTime = 0;
       noteMi.play()
       break;
 
     case 114 :
     case 1082 :
-      audio.currentTime = 0;
       noteFa.play()
       break;
 
     case 116 :
     case 1077 :
-      audio.currentTime = 0;
       noteSol.play()
       break;
 
     case 121 :
     case 1085 :
-      audio.currentTime = 0;
       noteLja.play()
       break;
 
     case 117 :
     case 1075 :
-      audio.currentTime = 0;
       noteSi.play()
       break;
 
