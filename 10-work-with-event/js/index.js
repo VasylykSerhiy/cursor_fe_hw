90beb189e5a18ef7ef8fc955e7e92cb0e025f9db
Serhii <vasylyk29@gmail.com>
2020-03-05 12:27:10 +0200

    fix

--- 10-work-with-event/js/index.js
+++ 10-work-with-event/js/index.js
@@ -18,36 +18,43 @@ document.addEventListener('keypress', (event) => {
   switch(event.keyCode) {
     case 113 :
     case 1081 :
+      audio.currentTime = 0;
       noteDo.play()
       break;
 
     case 119 :
     case 1094 :
+      audio.currentTime = 0;
       noteRe.play()
       break;
 
     case 101 :
     case 1091 :
+      audio.currentTime = 0;
       noteMi.play()
       break;
 
     case 114 :
     case 1082 :
+      audio.currentTime = 0;
       noteFa.play()
       break;
 
     case 116 :
     case 1077 :
+      audio.currentTime = 0;
       noteSol.play()
       break;
 
     case 121 :
     case 1085 :
+      audio.currentTime = 0;
       noteLja.play()
       break;
 
     case 117 :
     case 1075 :
+      audio.currentTime = 0;
       noteSi.play()
       break;
 
