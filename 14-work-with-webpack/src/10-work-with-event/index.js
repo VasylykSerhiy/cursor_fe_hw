b7550c3b659e8064ea9da971a8c48b26e8a9bf37
Serhii <vasylyk29@gmail.com>
2020-06-02 16:39:50 +0300

    fix

--- 14-work-with-webpack/src/10-work-with-event/index.js
+++ 14-work-with-webpack/src/10-work-with-event/index.js
@@ -1,6 +1,6 @@
 export let audio = null;
 
 export function playAudio() {
-  audio = new Audio("./fa.mp3");
+  audio = new Audio("../../src/10-work-with-event/fa.mp3");
   audio.play();
 }
--- 14-work-with-webpack/src/12-work-with-API/index.js
+++ 14-work-with-webpack/src/12-work-with-API/index.js
@@ -2,10 +2,6 @@
 const ress = document.querySelector('.result')
 const _apiBase = 'http://swapi.dev/api';
 export let pages = 1;
-let nextPages = true;
-let prevPages = true;
-let wookiee = false;
-
 
 export const renderPlanets = async (pages) => {
   ress.innerHTML= ''
@@ -13,60 +9,28 @@ export const renderPlanets = async (pages) => {
     .then(function (res) {
       const items = res.data.results;
      
-      if (wookiee) {
-        let url = []
-        res.data.results.map(item => url.push(item.url))
-
-        let requests = url.map(url => axios.get(`${url}?format=wookiee`))
-        Promise.all(requests)
-          .then(res => {
-            res.forEach(item => {
-              ress.classList.add('planet')
-              ress.innerHTML += (`
-                <div class="planet__wrap">
-                  <h4 class="planet__name">${item.data.whrascwo}</h4>
-                  <ul class="planet__detail">
-                    <li class="planet__detail__item">
-                      <span class="term">Population:</span>
-                      <span>${item.data.akooakhuanraaoahoowh}</span>
-                    </li>
-                    <li class="planet__detail__item">
-                      <span class="term">Rotation Period:</span>
-                      <span>${item.data.rcooaoraaoahoowh_akworcahoowa}</span>
-                    </li>
-                    <li class="planet__detail__item">
-                      <span class="term">Diameter:</span>
-                      <span>${item.data.waahrascwoaoworc}</span>
-                    </li>
-                  </ul>
-                </div>
-            `)
-            })
-          })
-      } else {
-        items.map((item) => {
-          ress.classList.add('planet')
-          ress.innerHTML += (`
-              <div class="planet__wrap">
-                <h4 class="planet__name">${item.name}</h4>
-                <ul class="planet__detail">
-                  <li class="planet__detail__item">
-                    <span class="term">Population:</span>
-                    <span>${item.population}</span>
-                  </li>
-                  <li class="planet__detail__item">
-                    <span class="term">Rotation Period:</span>
-                    <span>${item.rotation_period}</span>
-                  </li>
-                  <li class="planet__detail__item">
-                    <span class="term">Diameter:</span>
-                    <span>${item.diameter}</span>
-                  </li>
-                </ul>
-              </div>
-          `)
-        })
-      }
+      items.map((item) => {
+        ress.classList.add('planet')
+        ress.innerHTML += (`
+            <div class="planet__wrap">
+              <h4 class="planet__name">${item.name}</h4>
+              <ul class="planet__detail">
+                <li class="planet__detail__item">
+                  <span class="term">Population:</span>
+                  <span>${item.population}</span>
+                </li>
+                <li class="planet__detail__item">
+                  <span class="term">Rotation Period:</span>
+                  <span>${item.rotation_period}</span>
+                </li>
+                <li class="planet__detail__item">
+                  <span class="term">Diameter:</span>
+                  <span>${item.diameter}</span>
+                </li>
+              </ul>
+            </div>
+        `)
+      })
     })
 }
 
